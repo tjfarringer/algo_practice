652b4ec154993044f4e87478aa581c55cca6c4a5
Talmadge Farringer <tjfarrin@gmail.com>
2019-11-28 12:00:58 -0500

    add gitignore

---
+++ quick_sort.py
@@ -0,0 +1,23 @@
+
+
+
+import time
+from random import randint
+
+# quicksort key idea:  partition the array around a pivot element
+# step 1 - pick pivot element
+# step 2 - rearrange array so that:
+# step 2a - left side of pivot is less than the pivot element
+# step 2b - right side of pivot is greater than the pivot element
+# run time is O(n)
+
+A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7, 2]
+
+for x in range(0, 100000):
+    A.append(randint(-100, 100))
+
+
+
+
+
+
