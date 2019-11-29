8f9a436b57f1d6eb61952d82c2a8337297b08baa
Talmadge Farringer <tjfarrin@gmail.com>
2019-11-29 12:21:55 -0500

    inital merge sort commit

---
+++ merge_sort.py
@@ -0,0 +1,34 @@
+
+
+
+
+# merge sort key idea:
+# step 1 - recursively sort 1st half
+# step 2 - recursively sort 2nd half
+# traverse each step using pointers to merge the halves
+
+A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7, 2]
+
+for x in range(0, 10000):
+    A.append(randint(-10, 10))
+
+
+
+def partition(array, low, high):
+    '''
+    split the list into two blocks
+
+    :param array:
+    :param low:
+    :param high:
+    :return:
+    '''
+
+
+
+
+
+
+
+
+
