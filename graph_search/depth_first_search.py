02eea70504f7d008a5d72f157bf2319402e192e9
Talmadge Farringer <tjfarrin@gmail.com>
2019-12-07 15:35:01 -0500

    finish loop one

--- graph_search/depth_first_search.py
+++ graph_search/depth_first_search.py
@@ -12,6 +12,29 @@ class graph:
         # default dictionary to store graph
         self.graph = defaultdict(list)
 
+    def return_keys(self):
+        return self.graph.keys()
+
+    def return_values(self):
+        return self.graph.values()
+
+    def return_all_nodes(self):
+        all_nodes = set()
+
+        for k in graph.return_keys(self):
+            all_nodes.add(k)
+
+        for v in graph.return_values(self):
+            # if a node has multiple connections -- that will be a list
+            # need to parse through the list
+            if type(v) == list:
+                for x in v:
+                    all_nodes.add(x)
+            else:
+                all_nodes.add(v)
+
+        return all_nodes
+
     def addEdge(self, u, v):
         '''
         Add edge from u to v
@@ -21,9 +44,10 @@ class graph:
         '''
         self.graph[u].append(v)
 
-    def DFS_search(self, node, visited: list):
+    def DFS_search(self, node, visited: dict, finish_time_dict: dict, finish_order_list: list):
         '''
-        Do a depth-first search starting with the node provided. Visited is a dictionary that informs us if the node has been visited in the past.
+        Do a depth-first search starting with the node provided.
+        Visited is a dictionary that informs us if the node has been visited in the past.
 
         :param node:  Node to start the search from
         :param visited:  Dictionary showing if a node has already been visited
@@ -32,26 +56,58 @@ class graph:
 
         # mark the current node as visited
         visited[node] = True
-        print('Just visited: ', node)
+        print('At: ', node)
+
+        # If there is no next node, increment current_finish_time by one and add to the dict
+        if len(self.graph[node]) == 0:
+            print('at: ', node, ' but no linked objects')
+            print('current_finish_time is: ', len(finish_order_list))
+            finish_time_dict[len(finish_order_list)] = node
+            finish_order_list.append(node)
+            print('current_finish_time is: ', len(finish_order_list))
 
         # search for unvisited nodes connected to a previously visited node
         for next_node in self.graph[node]:
             print('Current node: ', node, ' Possible next node: ', next_node)
             if visited[next_node] == False:
-                print('Visited: ', next_node)
+                # print('Visited: ', next_node)
                 # dig into the next node
-                self.DFS_search(next_node, visited)
+                self.DFS_search(next_node, visited, finish_time_dict, finish_order_list)
+
+                # if that was the last node, increment current_finish_time by one and add to the dict
+                if next_node == self.graph[node][len(self.graph[node]) - 1]:
+                    print('at: ', node, ' but no unvisited objects - last loop')
+                    print('current_finish_time is: ', len(finish_order_list))
+                    finish_time_dict[len(finish_order_list)] = node
+                    finish_order_list.append(node)
+                    print('current_finish_time is: ', len(finish_order_list))
+
+            # if there are no unvisited nodes, increment current_finish_time by one and add to the dict
+            elif next_node == self.graph[node][len(self.graph[node])-1]:
+                print('at: ', node, ' but no unvisited objects')
+                print('current_finish_time is: ', len(finish_order_list))
+                finish_time_dict[len(finish_order_list)] = node
+                finish_order_list.append(node)
+                print('current_finish_time is: ', len(finish_order_list))
 
     def DFS(self, node):
         '''
         Main driver
         '''
         # Mark all the vertices as not visited
-        visited = [False] * (len(self.graph))
+        visited = dict()
+        finish_time_dict = dict()
+        current_finish_time = 0
+
+        for x in self.return_all_nodes():
+            visited[x] = False
 
         # Call the recursive helper function
         # to print DFS traversal
-        self.DFS_search(node, visited)
+        self.DFS_search(node, visited, finish_time_dict, current_finish_time)
+        print(visited)
+
+
 
 
 def main():
--- graph_search/kosaraju_two_pass_algo.py
+++ graph_search/kosaraju_two_pass_algo.py
@@ -3,19 +3,62 @@ from __future__ import absolute_import
 # # Import graph class
 from depth_first_search import graph
 
-# creating a test graph + searching through it
-g = graph()
-g.addEdge(1, 7)
-g.addEdge(7, 4)
-g.addEdge(4, 1)
-g.addEdge(7, 9)
-g.addEdge(9, 6)
-g.addEdge(6, 3)
-g.addEdge(3, 9)
-g.addEdge(6, 8)
-g.addEdge(8, 2)
-g.addEdge(2, 5)
-g.addEdge(5, 8)
+
+def reverse_graph(graph_to_reverse: dict) -> dict:
+    '''
+    This function reverses all the edges in a graph.  Required for step 1 of this algo.
+    :param graph:
+    '''
+    reversed_graph = graph()
+
+    for k in graph_to_reverse.graph:
+        for v in graph_to_reverse.graph[k]:
+            reversed_graph.addEdge(v, k)
+
+    return reversed_graph
+
+def DFS_loop(graph):
+    # Mark all the vertices as not visited
+    visited = dict()
+    finish_time_dict = dict()
+    finish_order_list = []
+
+    for x in graph.return_all_nodes():
+        visited[x] = False
+
+    for x in graph.return_all_nodes():
+        if visited[x] == False:
+            graph.DFS_search(x, visited, finish_time_dict, finish_order_list)
+
+    print(visited)
+    print(finish_time_dict)
+    magical_order = finish_time_dict
 
 
-print(g)
+    current_parent_node =
+
+
+def main():
+    # creating a test graph
+    g = graph()
+    g.addEdge(7, 1)
+    g.addEdge(4, 7)
+    g.addEdge(1, 4)
+    g.addEdge(9, 7)
+    g.addEdge(6, 9)
+    g.addEdge(3, 6)
+    g.addEdge(9, 3)
+    g.addEdge(8, 6)
+    g.addEdge(2, 8)
+    g.addEdge(5, 2)
+    g.addEdge(8, 5)
+
+    # reverse the graph
+    g_reverse = reverse_graph(g)
+
+    DFS_loop(g_reverse)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
