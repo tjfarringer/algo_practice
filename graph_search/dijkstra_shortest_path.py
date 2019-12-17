7b46b17707cdfe6611563dba92b3ffeff20e6061
Talmadge Farringer <tjfarrin@gmail.com>
2019-12-17 08:54:48 -0500

    adding ability to create graph from text file

---
+++ graph_search/dijkstra_shortest_path.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from collections import defaultdict
+
+# # Import graph class
+from graph_utility import Graph, create_graph_from_txt_file
+
+
+def dijkstra_shortest_path(graph, inital_element):
+    processed_vertex = set()
+    distances = {}
+
+    # update data objects for inital element
+    processed_vertex.add(inital_element)
+    distances[inital_element] = 0
+
+    while processed_vertex != graph.return_all_nodes():
+
+# create_graph_from_txt_file('/Users/talmadgefarringer/Downloads/dijkstraData.txt')
---
+++ graph_search/graph_utility.py
@@ -0,0 +1,64 @@
+
+class Vertex:
+    def __init__(self, node):
+        self.id = node
+        self.adjacent = {}
+
+    def add_neighbor(self, neighbor, weight=0):
+        self.adjacent[neighbor] = weight
+
+    def get_connections(self):
+        return self.adjacent.keys()
+
+    def get_id(self):
+        return self.id
+
+    def get_weight(self, neighbor):
+        return self.adjacent[neighbor]
+
+class Graph:
+    def __init__(self):
+        self.vertex_dict = {}
+        self.num_vertices = 0
+
+    def __iter__(self):
+        return iter(self.vertex_dict.values())
+
+    def add_vertex(self, node):
+        self.num_vertices += 1
+        new_vertex = Vertex(node)
+        self.vertex_dict[node] = new_vertex
+        return new_vertex
+
+    def get_vertex(self, n):
+        if n in self.vertex_dict:
+            return self.vertex_dict[n]
+        else:
+            return None
+
+    def add_edge(self, start, end, cost=0):
+        if start not in self.vertex_dict:
+            self.add_vertex(start)
+        if end not in self.vertex_dict:
+            self.add_vertex(end)
+
+        self.vertex_dict[start].add_neighbor(self.vertex_dict[end], cost)
+        self.vertex_dict[end].add_neighbor(self.vertex_dict[start], cost)
+
+    def get_vertices(self):
+        return self.vert_dict.keys()
+
+#
+def create_graph_from_txt_file(txt_file_location):
+
+    g = Graph
+
+    i = 0
+    f = open(txt_file_location, 'r')
+    for line in f:
+        split_line = line.split()
+        vertex_number = split_line[0]
+
+        for
+
+    print(i)
